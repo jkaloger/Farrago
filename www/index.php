2ab18a545393ecb89daf943a43a6d58d049bec3a
jkaloger <jkaloger@users.noreply.github.com>
2017-03-02 12:51:30 +1100

    "initial html"

---
+++ www/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <div class="container">
+        <header></header>
+        <div>
+            <ul class="nav">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+    
+</body>
+</html>
