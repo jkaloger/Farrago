adea760cf8ae764b0c62018c1eb407f662cd1f19
jkaloger <jkaloger@users.noreply.github.com>
2017-03-19 22:11:41 +1100

    mobile menu

--- proto/js/hamburger.js
+++ proto/js/hamburger.js
@@ -1,6 +1,10 @@
 $(function() {
 	var burger = $('.hamburger');
+	var nav = $('.mobile-menu');
 	burger.click(function() {
 		burger.toggleClass('hamburgerActive');
+		nav.toggleClass('pullUpMenu');
 	});
+
+	
 });
